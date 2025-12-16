5409701a5e48d84fad3f5aac9a5b15abcc2fe6be
Jafar Korchagin <korjafar@gmail.com>
2025-12-16 14:53:43 +0300

    minor updates

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -5,7 +5,6 @@ from aiogram.types import BotCommand
 
 async def set_main_menu():
     main_menu = [
-        BotCommand(command="help", description="❔  Вопросы (FAQ)"),
         BotCommand(command="support", description="🛠️  Техподдержка"),
     ]
     await bot.set_my_commands(main_menu)
